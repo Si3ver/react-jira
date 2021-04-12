e0f3b16d0e6710cd25aabfde4e2dbe5cc2b1eecc
Si3ver <izhouwl@163.com>
2021-04-13 00:07:56 +0800

    chore: 给 useDebounce 添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -27,8 +27,7 @@ export const useMount = (callback: () => void) => {
 };
 
 // 变量节流
-// TODO 这里后面用泛型重构
-export const useDebounce = (value: unknown, delay?: number): any => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
