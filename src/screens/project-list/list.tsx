5be2534960fd9f082dc6307c468fc423d58e9c6b
Si3ver <izhouwl@163.com>
2021-05-09 17:22:10 +0800

    feat: 添加路由

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -4,9 +4,12 @@
  * 根据传入的 list、users 渲染一个表格
  */
 import { User } from "screens/project-list/search-panel";
-import { Table, TableProps } from "antd";
+import { Table } from "antd";
 import dayjs from "dayjs";
+import { TableProps } from "antd/es/table";
+import { Link } from "react-router-dom";
 
+// TODO 把所有ID都改成number类型
 export interface Project {
   id: string;
   name: string;
@@ -28,8 +31,10 @@ export const List = ({ users, ...props }: ListProps) => {
       columns={[
         {
           title: "名称",
-          dataIndex: "name",
           sorter: (a, b) => a.name.localeCompare(b.name),
+          render(value, project) {
+            return <Link to={String(project.id)}>{project.name}</Link>;
+          },
         },
         {
           title: "部门",
