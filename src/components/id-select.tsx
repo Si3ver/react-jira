49880a8936c1ba6983d40720239b88602ed6eddb
si3ver <izhouwl@163.com>
2021-10-04 12:13:09 +0800

    feat: 添加id-select

---
+++ src/components/id-select.tsx
@@ -0,0 +1,37 @@
+import { Raw } from "types";
+import { Select } from "antd";
+
+type SelectProps = React.ComponentProps<typeof Select>;
+
+interface IdSelectProps
+  extends Omit<SelectProps, "value" | "onChange" | "options"> {
+  value: Raw | null | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: { name: string; id: number }[];
+}
+
+export const IdSelect = ({
+  value,
+  onChange,
+  defaultOptionName,
+  options,
+  ...restProps
+}: IdSelectProps) => {
+  return (
+    <Select
+      value={toNumber(value)}
+      onChange={(value) => onChange(toNumber(value) || undefined)}
+      {...restProps}
+    >
+      {defaultOptionName ? (
+        <Select.Option value={0}>{defaultOptionName}</Select.Option>
+      ) : null}
+      {options?.map((option) => (
+        <Select.Option value={option.id}>{option.name}</Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
